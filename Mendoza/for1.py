e6d3b7dd741544d94d0a7ca38762433b51d00979
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-14 19:09:05 -0500

    BUCLE PARA 1

---
+++ Mendoza/for1.py
@@ -0,0 +1,14 @@
+#Programa que muestre los numeros desde -20 hasta 15
+# y luego desde 15 hasta -20
+i=-20
+n=15
+while(i<=20):
+    print(i)
+    i=i+1
+#fin_while
+
+while(n>-16):
+    print(n)
+    n=n-1
+#fin_while
+

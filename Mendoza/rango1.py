53da5ac6110eb659d1d9a99a5e3b42e88205fac0
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.combucle>
2019-12-15 21:33:29 -0500

    BUCLE ITERACION DE RANGON

---
+++ Mendoza/rango1.py
@@ -0,0 +1,11 @@
+#Programa que pida al usuario una palabra
+#y la muestre 10 veces por pantalla.
+#Declaracion
+palabra=""
+
+#Input
+palabra=input("Introduce una palabra: ")
+
+for i in range(10):
+    print(palabra)
+#fin_for

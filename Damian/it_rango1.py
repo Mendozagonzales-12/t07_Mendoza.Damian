4ca13b2737ae8ad57342156675c143e05358291a
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-16 21:28:18 -0500

    BUCLE DE ITERACION CON RANGO 1

---
+++ Damian/it_rango1.py
@@ -0,0 +1,11 @@
+#programa que pida una frase al usuario
+#la muestra  5 veces en pantalla
+#Declaracion
+frase=""
+
+#input
+frase=input("introduce una frase:")
+
+for i in range(5):
+    print(frase)
+#fin_for

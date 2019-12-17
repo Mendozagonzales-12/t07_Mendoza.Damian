6857f2888c7d7561cbfb328ebc6ad56fe6dafc82
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-16 21:29:42 -0500

    BUCLE DE ITERACION CON RANGO 2

---
+++ Damian/it_rango2.py
@@ -0,0 +1,11 @@
+#programa que pida una asignacion al usuario
+#la muestra  15 veces en pantalla
+#Declaracion
+asignacion=""
+
+#input
+asignacion=input("introduce una asignacion:")
+
+for i in range(15):
+    print(asignacion)
+#fin_for

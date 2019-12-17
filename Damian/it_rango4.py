8628e699618f3bd87599c6c105c838953b255740
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-16 21:31:51 -0500

    BUCLE DE ITERACION CON RANGO 4

---
+++ Damian/it_rango4.py
@@ -0,0 +1,11 @@
+#programa que pida una nota al usuario
+#la muestra  7 veces en pantalla
+#Declaracion
+nota=0
+
+#input
+nota=input("introduce una nota:")
+
+for i in range(7):
+    print(nota)
+#fin_for

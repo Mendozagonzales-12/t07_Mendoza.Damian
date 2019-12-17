8893108bd7fe90569616255188a8dab0f1ae9c46
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-16 21:32:49 -0500

    BUCLE DE ITERACION CON RANGO 5

---
+++ Damian/it_rango5.py
@@ -0,0 +1,11 @@
+#programa que pida nombre del alumno
+#la muestra  2 veces en pantalla
+#Declaracion
+nombre_alumno=""
+
+#input
+nombre_alumno=input("introduce un nombre:")
+
+for i in range(2):
+    print(nombre_alumno)
+#fin_for
---
+++ lote.bat
@@ -0,0 +1,19 @@
+python Mendoza/iteracion1.py "HOLA"
+python Mendoza/iteracion1.py "DIME"
+python Mendoza/iteracion1.py "CHAU"
+
+python Mendoza/iteracion2.py 0
+python Mendoza/iteracion2.py 1
+python Mendoza/iteracion2.py 2
+
+python Mendoza/iteracion3.py "T"
+python Mendoza/iteracion3.py "I"
+python Mendoza/iteracion3.py "A"
+
+python Mendoza/iteracion4.py "DESAYUNO"
+python Mendoza/iteracion4.py "ALMUERZO"
+python Mendoza/iteracion4.py "CENA"
+
+python Mendoza/iteracion5.py 109 76
+python Mendoza/iteracion5.py 3 5
+python Mendoza/iteracion5.py 45 45

40b67632a011e8748aa68a63ab9975fa82eab889
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-16 21:30:52 -0500

    BUCLE DE ITERACION CON RANGO 3

---
+++ Damian/it_rango3.py
@@ -0,0 +1,11 @@
+#programa que pida una sentencia al usuario
+#la muestra  10 veces en pantalla
+#Declaracion
+sentencia=""
+
+#input
+sentencia=input("introduce una sentencia:")
+
+for i in range(10):
+    print(sentencia)
+#fin_for
